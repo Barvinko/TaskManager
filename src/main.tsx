83a2a1475793b901f8b803cfa902ed9ba7835905
Barvinko <vlad.barvinko@gmail.com>
2026-01-29 01:03:35 +0200

    feat: set up a full-fledged MSW server simulation

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './store/store';
-import './index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style/index.scss';
 
 async function enableMocking() {
+  console.log('Current Mode:', import.meta.env.MODE);
   if (import.meta.env.MODE !== 'development') {
     return;
   }
 
   const { worker } = await import('./mocks/browser');
   return worker.start({
+    serviceWorker: {
+      url: '/mockServiceWorker.js',
+    },
     onUnhandledRequest: 'bypass',
   });
 }
